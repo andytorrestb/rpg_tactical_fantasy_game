66bcf10178c928f81be89e908f0c60577e60e907
andytorrestb <andytorrestb@gmail.com>
2025-11-21 22:19:46 -0500

    Update data/fr/text.py
    Co-authored-by: Grimmys <funkyzermmy@gmail.com>

--- data/fr/text.py
+++ data/fr/text.py
@@ -227,7 +227,7 @@ STR_YOU_HAVE_NO_KEY_TO_OPEN_A_CHEST = "Vous n'avez pas de clé pour ouvrir un co
 STR_ITEM_HAS_BEEN_TRADED = "L'objet a été échangé"
 STR_ITEM_HAS_BEEN_THROWN_AWAY = "L'objet a été jeté"
 STR_THE_ITEM_CANNOT_BE_UNEQUIPPED_NOT_ENOUGH_SPACE_IN_UR_INVENTORY = (
-    "L'objet ne peut pas être déséquipé : il n'y a pas assez d'espace dans ton inventaire."
+    "L'objet ne peut pas être déséquipé : il n'y a pas assez d'espace dans votre inventaire."
 )
 STR_THE_ITEM_HAS_BEEN_UNEQUIPPED = "L'objet a été déséquipé"
 STR_THE_ITEM_HAS_BEEN_EQUIPPED = "L'objet a été équipé"
